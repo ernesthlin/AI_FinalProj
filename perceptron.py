a25633d899ddc87f872ab1343ac29c641fe4e6e2
Nicolas Carchio <ncarchio@gmail.com>
2019-11-25 12:25:00 -0500

    predict added, before testing

--- perceptron.py
+++ perceptron.py
@@ -21,9 +21,9 @@ class Perceptron():
 		return flattened
 
 ##########################################################################################################
-											'''
-										Face Functions  		
-											'''
+										
+										#Face Functions  		
+										
 ##########################################################################################################
 	'''
 		calculates 
@@ -136,81 +136,92 @@ class Perceptron():
 		return false
 
 ##########################################################################################################
-											'''
-										Digit Functions  		
-											'''
+											
+										# Digit Functions  		
+											
 ##########################################################################################################
-'''
-	Calculate f values for each digit 0-9 and take the max value
-		f(xi, weights) = w0*o(xi) + w1*o(xi) + w2*o(xi)... + w4071*o(xi) + w4072
-	Self.weights is a 2D array of size 10 with indeces 0-9 representing those digits, with thier cooresponding list 
-	equal to the weights for that digit 
-	Returns the index of the max f value, or the 'digit' that we have guessed with the highest certainty 
-'''
-def calculate_f_digit(self, xi): 
-	f_digits = []
-	for i in range(len(f_digits)):
-		sum = 0
-		for j in range(len(xi)):
-			sum += (self.digit_weights[i][j] * xi[j])
-		sum += self.digit_weights[i][-1]
-		f_digits[i] = sum
-	return f_digits.index(max(f_digits))
+	'''
+		Calculate f values for each digit 0-9 and take the max value
+			f(xi, weights) = w0*o(xi) + w1*o(xi) + w2*o(xi)... + w4071*o(xi) + w4072
+		Self.weights is a 2D array of size 10 with indeces 0-9 representing those digits, with thier cooresponding list 
+		equal to the weights for that digit 
+		Returns the index of the max f value, or the 'digit' that we have guessed with the highest certainty 
+	'''
+	def calculate_f_digit(self, xi): 
+		f_digits = []
+		for i in range(len(f_digits)):
+			sum = 0
+			for j in range(len(xi)):
+				sum += (self.digit_weights[i][j] * xi[j])
+			sum += self.digit_weights[i][-1]
+			f_digits[i] = sum
+		return f_digits.index(max(f_digits))
 
-'''
-	If correct guess, move on. 
-	If incorrect guess, 2 things must happen: 
-		1. The incorrect guess's weights must be either decremented by each of its features
-		2. The weights for the correct digit's weights must be incremented, the opposite of step 1 
-'''
-def check_guess_accuracy_and_update(self, guess, index, xi): 
-	if self.y_values[index] != guess: 
-		# decrement incorrect 
-		self.digit_weights[guess] = [self.digit_weights[guess][i] - xi[i] for i in range(len(self.digit_weights[guess]))-1]
-		self.digit_weights[guess][-1] = 1 - self.digit_weights[guess][-1]
+	'''
+		If correct guess, move on. 
+		If incorrect guess, 2 things must happen: 
+			1. The incorrect guess's weights must be either decremented by each of its features
+			2. The weights for the correct digit's weights must be incremented, the opposite of step 1 
+	'''
+	def check_guess_accuracy_and_update(self, guess, index, xi): 
+		if self.y_values[index] != guess: 
+			# decrement incorrect 
+			self.digit_weights[guess] = [self.digit_weights[guess][i] - xi[i] for i in range(len(self.digit_weights[guess]))-1]
+			self.digit_weights[guess][-1] = 1 - self.digit_weights[guess][-1]
 
-		# increment correct
-		self.digit_weights[index] = [self.digit_weights[index][i] + xi[i] for i in range(len(self.digit_weights[index]))-1]
-		self.digit_weights[index][-1] += 1
-		return False
-	return True
+			# increment correct
+			self.digit_weights[index] = [self.digit_weights[index][i] + xi[i] for i in range(len(self.digit_weights[index]))-1]
+			self.digit_weights[index][-1] += 1
+			return False
+		return True
 
-def percentage_correct_digit(self, guesses): 
-	correct_zipped = list(zip(self.y_values, guesses))
-	correct = [i for i, j in correct_zipped if i == j]
-	return (len(correct)/len(self.y_values)) * 100
+	def percentage_correct_digit(self, guesses): 
+		correct_zipped = list(zip(self.y_values, guesses))
+		correct = [i for i, j in correct_zipped if i == j]
+		return (len(correct)/len(self.y_values)) * 100
 
-'''
-	weights will be of size 4720 + 1 (the w0 is the extra weight)
-	Length of training set (X_train is 451 (usually) aka how many images we have)
-		- our features are each individual pixel, and each pixel has either a 0, 1, or 2 (for numbers) 
-		- f(xi, weights) = w0 + w1*o(xi) + w2*o(xi) + w3*o(xi)... + w4072*o(xi)
-'''
-def train_digit(self, X_train): 
-	pixels = 28 * 29 # digit image pixels
-	# get flattened list
-	flattened = flatten_list(self, X_train)
-	# initialize the weights list, to be 1 more than the # pixels in the image
-	for i in range(10)
-		self.digit_weights[i] = [1 for j in range(pixels+1)]
+	'''
+		weights will be of size 4720 + 1 (the w0 is the extra weight)
+		Length of training set (X_train is 451 (usually) aka how many images we have)
+			- our features are each individual pixel, and each pixel has either a 0, 1, or 2 (for numbers) 
+			- f(xi, weights) = w0 + w1*o(xi) + w2*o(xi) + w3*o(xi)... + w4072*o(xi)
+	'''
+	def train_digit(self, X_train): 
+		pixels = 28 * 29 # digit image pixels
+		# get flattened list
+		flattened = flatten_list(self, X_train)
+		# initialize the weights list, to be 1 more than the # pixels in the image
+		for i in range(10): 
+			self.digit_weights[i] = [1 for j in range(pixels+1)]
 
-	flattened_index = 0
-	guesses = []
-	for i in range(len(X_train)): 
-		current_image =[]
-		for j in range(pixels): 
-			current_image.append(flattened[flattened_index])
-			flattened_index += 1			
-		# Calculate the f value and assign the 'digit' we have guessed to guess
-		guess = calculate_f_digit(self, current_image)
-		check = check_guess_accuracy_and_update(self, guess, i, current_image)
-		while (check is False): 
+		flattened_index = 0
+		guesses = []
+		for i in range(len(X_train)): 
+			current_image =[]
+			for j in range(pixels): 
+				current_image.append(flattened[flattened_index])
+				flattened_index += 1			
+			# Calculate the f value and assign the 'digit' we have guessed to guess
 			guess = calculate_f_digit(self, current_image)
 			check = check_guess_accuracy_and_update(self, guess, i, current_image)
-		guesses[i] = guess
-	percent_correct = percentage_correct_digit(self, guesses)
-	return percent_correct
+			while (check is False): 
+				guess = calculate_f_digit(self, current_image)
+				check = check_guess_accuracy_and_update(self, guess, i, current_image)
+			guesses[i] = guess
+		percent_correct = percentage_correct_digit(self, guesses)
+		return percent_correct
+
+	def predict_digit(self, x_input): 
+		# assign x and y values, flatten the list of lists
+		x, y = x_input
+		flattened = [j for i in x for j in i]
+
+		digit = calculate_f_digit(self, flattened)
+
+		if digit == y: 
+			return true
+		else: 
+			return false
 
 
-
-		
+			
